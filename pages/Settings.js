4a3066c0a36f892c42a093b63161e6c360566043
kailous <kailous@live.cn>
2023-05-01 17:03:55 +0800

    添加设置

--- pages/Settings.js
+++ pages/Settings.js
@@ -1,4 +1,4 @@
-// pages/Settings.js 用于设置环境变量
+// pages/Settings.js
 
 import { useState, useEffect } from 'react';
 
@@ -12,17 +12,9 @@ function Settings() {
     const [errorMessage, setErrorMessage] = useState('');
 
     useEffect(() => {
-        async function fetchEnvVars() {
-            try {
-                const res = await fetch('/api/update-env-vars');
-                const data = await res.json();
-                setEnvVars(data);
-            } catch (err) {
-                setErrorMessage('获取环境变量失败');
-            }
-        }
-
-        fetchEnvVars();
+        // 从本地存储获取环境变量
+        const storedEnvVars = JSON.parse(localStorage.getItem('envVars')) || {};
+        setEnvVars(storedEnvVars);
     }, []);
 
     const handleSubmit = async (event) => {
@@ -31,20 +23,9 @@ function Settings() {
         setErrorMessage('');
 
         try {
-            const res = await fetch('/api/update-env-vars', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(envVars),
-            });
-
-            if (!res.ok) {
-                const data = await res.json();
-                setErrorMessage(data.message || '更新环境变量失败');
-            } else {
-                setErrorMessage('环境变量更新成功');
-            }
+            // 将用户修改的环境变量存储到本地存储中
+            localStorage.setItem('envVars', JSON.stringify(envVars));
+            setErrorMessage('环境变量更新成功');
         } catch (err) {
             setErrorMessage('更新环境变量失败');
         }
